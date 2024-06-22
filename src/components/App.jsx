e56db3d1859385c605d2d7ac86cef3b9eb32559a
Ramon San Gabriel <jeshero11@gmail.com>
2024-06-22 10:17:48 +0800

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,13 +15,45 @@ export class App extends Component {
     name: '',
     number: '',
   };
+  addContact = newContact => {
+    this.setState(prevState => ({
+      contact: [...prevState.contact, newContact],
+    }));
+  };
   render() {
+    const { contact, addContact, newContact } = this.state;
+
     return (
       <div>
         <h2>Phonebook</h2>
-        <ContactForm name={this.name} id={this.id} />
+        <form action="">
+          <label>
+            <p>Name</p>
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan."
+              required
+            />
+          </label>
+          <label>
+            <p>Number</p>
+            <input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan."
+              required
+            />
+          </label>
+        </form>
+
+        <button onClick={this.addContact}>Add Contact</button>
+
         <h2>Contacts</h2>
-        <ContactListItem />
+
+        {/* <ContactListItem /> */}
       </div>
     );
   }
--- src/components/ContactListItem/ContactListItem.jsx
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -1,10 +1,1 @@
-export const ContactListItem = ({ contacts }) => {
-  // const { name, id } = this.state;
-  return (
-    <ul>
-      {contacts.map(contact => (
-        <li>{contact.name}</li>
-      ))}
-    </ul>
-  );
-};
+
