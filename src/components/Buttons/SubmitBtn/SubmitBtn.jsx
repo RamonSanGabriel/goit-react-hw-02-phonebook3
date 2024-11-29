1ad838b93c10713d7abef97b2eb071e0425a48a6
RamonSanGabriel <jeshero11@gmail.com>
2024-11-29 22:54:11 +0800

    removed unsed variables

--- src/components/Buttons/SubmitBtn/SubmitBtn.jsx
+++ src/components/Buttons/SubmitBtn/SubmitBtn.jsx
@@ -1,19 +1,6 @@
-import { useForm } from 'react-hook-form';
-
 import css from './SubmitBtn.module.css';
-import { useState } from 'react';
 
-let counter = 0;
 const SubmitBtn = () => {
-  const [value, setValue] = useState('');
-  const [result, setResult] = useState('');
-
-  const handleChange = e => {
-    e.preventDefault();
-    setValue(e.target.value);
-    setResult('');
-    // counter++;
-  };
   const onSubmit = data => console.log('Form submitted', data);
   return (
     <div>
